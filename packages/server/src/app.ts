bda2d75fced0085ada64a1266289ebea99786cd9
jongkeun-kim <kjkandrea@gmail.com>
2022-08-14 16:55:51 +0900

    feat. add lowdb

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,4 +1,18 @@
-import * as express from 'express';
+import express from 'express';
+import {join, dirname} from 'path';
+import {Low, JSONFile} from 'lowdb';
+import {fileURLToPath} from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Use JSON file for storage
+const file = join(__dirname, '../db/db.json');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+// Read data from JSON file, this will set db.data content
+await db.read();
+console.log(db);
 
 const app = express();
 
