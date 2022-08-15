f07d9ddbc36e9485cebe85efe642b892cd81f7e6
jongkeun-kim <kjkandrea@gmail.com>
2022-08-15 18:35:33 +0900

    chore. refactor throwObjectValueEmptyError test

--- packages/server/src/global/calculations/validation.test.ts
+++ packages/server/src/global/calculations/validation.test.ts
@@ -2,73 +2,89 @@ import {throwObjectTypeError, throwObjectValueEmptyError} from './validation';
 
 describe('validation', () => {
   describe('throwObjectTypeError()', () => {
-    it("{ foo: 123 }, { foo: 'string'} 이면 에러를 throw 한다.", () => {
-      expect(() =>
-        throwObjectTypeError(
-          {foo: 123},
-          {
-            foo: 'string',
-          }
-        )
-      ).toThrowError();
+    describe('에러를 throw 하는 케이스', () => {
+      it("{ foo: 123 }, { foo: 'string'}", () => {
+        expect(() =>
+          throwObjectTypeError(
+            {foo: 123},
+            {
+              foo: 'string',
+            }
+          )
+        ).toThrowError();
+      });
     });
 
-    it("{ foo: '123' }, { foo: 'string'} 이면 foo 는 string typeof 이므로 에러를 throw 하지 않는다.", () => {
-      expect(() =>
-        throwObjectTypeError(
-          {foo: '123'},
-          {
-            foo: 'string',
-          }
-        )
-      ).not.toThrowError();
-    });
+    describe('에러를 throw 하지 않는 케이스', () => {
+      it("{ foo: '123' }, { foo: 'string'}", () => {
+        expect(() =>
+          throwObjectTypeError(
+            {foo: '123'},
+            {
+              foo: 'string',
+            }
+          )
+        ).not.toThrowError();
+      });
 
-    it("{ foo: '123', bar: 123, baz: true }, { foo: 'string', bar: 'number', baz: 'boolean' } 이면 에러를 throw 하지 않는다.", () => {
-      expect(() =>
-        throwObjectTypeError(
-          {foo: '123', bar: 123, baz: true},
-          {foo: 'string', bar: 'number', baz: 'boolean'}
-        )
-      ).not.toThrowError();
+      it("{ foo: '123', bar: 123, baz: true }, { foo: 'string', bar: 'number', baz: 'boolean' }", () => {
+        expect(() =>
+          throwObjectTypeError(
+            {foo: '123', bar: 123, baz: true},
+            {foo: 'string', bar: 'number', baz: 'boolean'}
+          )
+        ).not.toThrowError();
+      });
     });
   });
 
   describe('throwObjectValueEmptyError()', () => {
-    it("{ foo: '' } 이면 에러를 throw 한다.", () => {
-      expect(() => throwObjectValueEmptyError({foo: ''})).toThrowError();
+    describe('에러를 throw 하는 케이스', () => {
+      it("{ foo: '' }", () => {
+        expect(() => throwObjectValueEmptyError({foo: ''})).toThrowError();
+      });
+
+      it('{ foo: null }', () => {
+        expect(() => throwObjectValueEmptyError({foo: null})).toThrowError();
+      });
+
+      it('{ foo: undefined }', () => {
+        expect(() =>
+          throwObjectValueEmptyError({foo: undefined})
+        ).toThrowError();
+      });
     });
 
-    it('{ foo: null } 이면 에러를 throw 한다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: null})).toThrowError();
-    });
+    describe('에러를 throw 하지 않는 케이스', () => {
+      it("{ foo: 'foo' }", () => {
+        expect(() =>
+          throwObjectValueEmptyError({foo: 'foo'})
+        ).not.toThrowError();
+      });
 
-    it('{ foo: undefined } 이면 에러를 throw 한다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: undefined})).toThrowError();
-    });
+      it('{ foo: 0 }', () => {
+        expect(() => throwObjectValueEmptyError({foo: 0})).not.toThrowError();
+      });
 
-    it("{ foo: 'foo' } 이면 에러를 throw 하지 않는다.", () => {
-      expect(() => throwObjectValueEmptyError({foo: 'foo'})).not.toThrowError();
-    });
+      it('{ foo: true }', () => {
+        expect(() =>
+          throwObjectValueEmptyError({foo: true})
+        ).not.toThrowError();
+      });
 
-    it('{ foo: 0 } 이면 에러를 throw 하지 않는다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: 0})).not.toThrowError();
-    });
+      it('{ foo: false }', () => {
+        expect(() =>
+          throwObjectValueEmptyError({foo: false})
+        ).not.toThrowError();
+      });
 
-    it('{ foo: true } 이면 에러를 throw 하지 않는다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: true})).not.toThrowError();
-    });
+      it('{ foo: {} }', () => {
+        expect(() => throwObjectValueEmptyError({foo: {}})).not.toThrowError();
+      });
 
-    it('{ foo: false } 이면 에러를 throw 하지 않는다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: false})).not.toThrowError();
-    });
-
-    it('{ foo: {} } 이면 에러를 throw 하지 않는다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: {}})).not.toThrowError();
-    });
-
-    it('{ foo: [] } 이면 에러를 throw 하지 않는다.', () => {
-      expect(() => throwObjectValueEmptyError({foo: []})).not.toThrowError();
+      it('{ foo: [] }', () => {
+        expect(() => throwObjectValueEmptyError({foo: []})).not.toThrowError();
+      });
     });
   });
 });
