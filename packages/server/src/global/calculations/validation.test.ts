5defba760c893b3820da9c4cca0c987ded0cce08
jongkeun-kim <kjkandrea@gmail.com>
2022-08-15 18:23:59 +0900

    feat. add throwObjectTypeError test

--- packages/server/src/global/calculations/validation.test.ts
+++ packages/server/src/global/calculations/validation.test.ts
@@ -2,8 +2,26 @@ import {throwObjectTypeError} from './validation';
 
 describe('validation', () => {
   describe('throwObjectTypeError()', () => {
-    it('is function', () => {
-      expect(typeof throwObjectTypeError).toBe('function');
+    it("{ foo: 123 }, { foo: 'string'} 이면 에러를 throw 한다.", () => {
+      expect(() =>
+        throwObjectTypeError(
+          {foo: 123},
+          {
+            foo: 'string',
+          }
+        )
+      ).toThrowError();
+    });
+
+    it("{ foo: '123' }, { foo: 'string'} 이면 foo 는 string typeof 이므로 에러를 throw 하지 않는다.", () => {
+      expect(() =>
+        throwObjectTypeError(
+          {foo: '123'},
+          {
+            foo: 'string',
+          }
+        )
+      ).not.toThrowError();
     });
   });
 });
