ee0cca323865198d048ad0bbcf8dfd5d08541124
jongkeun-kim <kjkandrea@gmail.com>
2022-08-16 09:20:25 +0900

    refactor

--- packages/server/src/global/actions/validation.ts
+++ packages/server/src/global/actions/validation.ts
@@ -4,6 +4,14 @@ type TypedObject<K extends PropertyKey> = {
   [key in K]: PrimitiveType;
 };
 
+type Entries<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+
+function entries<T>(obj: T): Entries<T> {
+  return Object.entries(obj) as any;
+}
+
 const messageTemplate = {
   typeError: (keyName: string) => `${keyName} 의 타입이 올바르지 않습니다.`,
   emptyError: (keyName: string) => `${keyName} 의 값이 빈 값입니다.`,
@@ -14,23 +22,28 @@ export const throwObjectTypeError = <T extends object>(
   typedObject: TypedObject<keyof T>
 ) => {
   const [problemKey] =
-    Object.entries(typedObject).find(([k, v]) => {
-      const currentValue = object[k as keyof T];
-      const primitiveType = v as PrimitiveType;
+    entries(typedObject).find(([k, primitiveType]) => {
+      const currentValue = object[k];
 
       return typeof currentValue !== primitiveType;
     }) ?? [];
 
-  if (problemKey) throw new Error(messageTemplate.typeError(problemKey));
+  if (problemKey)
+    throw new Error(messageTemplate.typeError(problemKey.toString()));
 };
 
 export const throwObjectValueEmptyError = <T extends object>(object: T) => {
   const [problemKey] =
-    Object.entries(object).find(([, v]) => {
+    entries(object).find(([, v]) => {
       if (Array.isArray(v)) return false;
-      const value = typeof v === 'string' ? v.trim() : v.toString();
+      if (['number', 'boolean'].includes(typeof v)) return false;
+
+      if (v === undefined || v === null) return true;
+
+      const value = typeof v === 'string' ? v.trim() : v;
       return !value;
     }) ?? [];
 
-  if (problemKey) throw new Error(messageTemplate.emptyError(problemKey));
+  if (problemKey)
+    throw new Error(messageTemplate.emptyError(problemKey.toString()));
 };
